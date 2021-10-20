5623cc51f47e4fb8adfc595d92c7bf88f101ea0b
Mahmoud <m.alhumsialrefaai@student.tudelft.nl>
2021-10-20 15:11:07 +0200

    Add react-router-dom + simple routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
 import React from 'react';
 import './App.css';
 import QR from './Components/QR';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <QR callback={(res) => { alert('Decode result: ' + res) }}/>
-      </header>
-    </div>
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/qr">QR Reader</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Switch>
+        <Route path="/qr">
+          <header className="App-header">
+            <QR callback={(res) => { alert('Decode result: ' + res) }}/>
+          </header>
+        </Route>
+        <Route path="/">
+          {/* <Home /> */}
+        </Route>
+      </Switch>
+  </Router>
   );
 }
 
