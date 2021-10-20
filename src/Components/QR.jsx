462c3ad972f16bb64ea23cb73b9e78adff96d68f
Mahmoud <m.alhumsialrefaai@student.tudelft.nl>
2021-10-20 15:10:38 +0200

    Stop qr scanner when components unmount

--- src/Components/QR.jsx
+++ src/Components/QR.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useRef } from 'react';
 import QrScanner from 'qr-scanner';
 
 // eslint-disable-next-line import/no-webpack-loader-syntax
@@ -7,11 +7,20 @@ QrScanner.WORKER_PATH = QrScannerWorkerPath;
 
 function QR({ callback = () => {} }) {
 
+  const qrScannerRef = useRef(null)
+
   const elRef = useCallback(node => {
-      if (node !== null) {
-        const qrScanner = new QrScanner(node, callback);
-        qrScanner.start();
-      }
+      if(node !== null && qrScannerRef.current === null)
+        qrScannerRef.current = new QrScanner(node, callback);
+
+      // https://reactjs.org/docs/refs-and-the-dom.html#callback-refs
+      // React will call the ref callback with the DOM element when the component mounts, and call it with null when it unmounts
+      
+      if (node !== null)
+        qrScannerRef.current.start();
+      else
+        qrScannerRef.current.stop();
+
   }, [callback]);
 
   return <video ref={elRef}></video>;
